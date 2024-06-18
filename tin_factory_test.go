b9fe78d853c1f9ab0d4c029fe34607473f8cb131
Menendez6 <pabloj.menendez@gmail.com>
2024-06-18 11:13:04 +0000

    test for factory

---
+++ tin_factory_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGetTinLookup(t *testing.T) {
+	tests := []struct {
+		name         string
+		countryCode  string
+		expectedType reflect.Type // The type we expect the return value to be
+	}{
+		{
+			name:         "European Country Code",
+			countryCode:  "ES",
+			expectedType: reflect.TypeOf((*VIESLookup)(nil)).Elem(),
+		},
+		{
+			name:         "Non-European Country Code",
+			countryCode:  "US",
+			expectedType: nil,
+		},
+		// Add more test cases as needed
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			validator := GetTinLookup(tt.countryCode)
+
+			if tt.expectedType == nil {
+				if validator != nil {
+					t.Errorf("Expected nil validator for country code %s, got %v", tt.countryCode, validator)
+				}
+			} else {
+				actualType := reflect.TypeOf(validator)
+				if actualType != tt.expectedType {
+					t.Errorf("Expected validator type %s for country code %s, got %s", tt.expectedType, tt.countryCode, actualType)
+				}
+			}
+		})
+	}
+}
